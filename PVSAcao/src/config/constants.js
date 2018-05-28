a20d51e0a4927870bce42ef25d4856255233df97
georgerappel <george.concei@hotmail.com>
2018-05-28 10:02:08 -0300

    Criado arquivo para constantes - lista de matérias etc

---
+++ PVSAcao/src/config/constants.js
@@ -0,0 +1,13 @@
+export default constantes = {
+    materias: [
+        //O nome pode ser alterado livremente, não altere o value porque
+        // ele é usado no banco de dados.
+        { nome: 'Biologia', value: 'biologia' },
+        { nome: 'Matemática', value: 'matematica' },
+        { nome: 'História', value: 'historia' },
+        { nome: 'Português', value: 'portugues' },
+        { nome: 'Geografia', value: 'geografia' },
+        { nome: 'Física', value: 'fisica' },
+        { nome: 'Química', value: 'quimica' }
+    ],
+};
